a4825ecce592004660775ac520837df012fe585f
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2020-12-20 17:16:23 +0100

    Add day 20: part 2 / step 1

--- src/day20.rs
+++ src/day20.rs
@@ -1,6 +1,7 @@
 use crate::utils::lines;
 use std::collections::HashMap;
 
+#[derive(Clone)]
 struct Tile {
     id: u64,
     tile: Vec<Vec<char>>,
@@ -85,7 +86,16 @@ fn count(tiles: &Vec<Tile>) -> HashMap<u32, u32> {
 }
 
 fn corners(index: &HashMap<u64, Vec<u32>>, count: &HashMap<u32, u32>) -> Vec<u64> {
-    index.iter()
+    let keys = {
+        let mut ks = index.keys()
+            .into_iter()
+            .cloned()
+            .collect::<Vec<_>>();
+        ks.sort();
+        ks
+    };
+    keys.iter()
+        .map(|k| (k, index.get(k).unwrap()))
         .filter(|(_, edges)| {
             let unique = edges.iter()
                 .map(|e| count.get(e).cloned().unwrap_or_default())
@@ -97,15 +107,130 @@ fn corners(index: &HashMap<u64, Vec<u32>>, count: &HashMap<u32, u32>) -> Vec<u64
         .collect()
 }
 
+fn adj(size: usize, grid: &Vec<u64>, idx: usize, index: &HashMap<u64, Vec<u32>>) -> Vec<Vec<u32>> {
+    let (row, col) = (idx / size, idx % size);
+
+    let mut result = Vec::with_capacity(2);
+    if col > 0 {
+        let id = grid.get(idx - 1).unwrap();
+        let edges = index.get(id).unwrap();
+        result.push(edges.clone());
+    }
+    if row > 0 {
+        let id = grid.get(idx - size).unwrap();
+        let edges = index.get(id).unwrap();
+        result.push(edges.clone());
+    }
+    result
+}
+
+fn align(size: usize, grid: Vec<u64>, queue: Vec<u64>, idx: usize, index: &HashMap<u64, Vec<u32>>, count: &HashMap<u32, u32>) -> Vec<u64> {
+    if grid.len() == size * size {
+        return grid;
+    }
+    if queue.is_empty() {
+        return vec![];
+    }
+    let (row, col) = (idx / size, idx % size);
+    let is_edge = col == 0 || col == size-1 || row == 0 || row == size-1;
+    //println!("align: idx={} is_edge={}, prev={}", idx, is_edge, grid[idx-1]);
+
+    let adj = adj(size, &grid, idx, index);
+    //println!("\tadj={:?}", adj);
+
+    let next = queue.iter()
+        .filter(|tile| {
+            let edges = index.get(tile).unwrap();
+            adj.iter()
+                .all(|es| es.iter().any(|e| edges.contains(e)))
+        })
+        .cloned()
+        .collect::<Vec<_>>();
+    let next = if !is_edge {
+        next
+    } else {
+        next.into_iter()
+            .filter(|id| {
+                let edges = index.get(id).unwrap();
+                edges.iter()
+                    .any(|e| *count.get(e).unwrap() == 1)
+            })
+            .collect()
+    };
+    //println!("\tnext={:?}", next);
+
+    next.into_iter()
+        .map(|tile| {
+            let mut g = grid.clone();
+            g.push(tile);
+
+            let rest = queue.iter()
+                .filter(|t| **t != tile)
+                .cloned()
+                .collect::<Vec<_>>();
+
+            align(size, g, rest, idx + 1, index, count)
+        })
+        .find(|vec| !vec.is_empty())
+        .unwrap_or_default()
+}
+
+fn crop(tile: Tile) -> Tile {
+    let n = tile.tile.len();
+
+    let cropped = tile.tile
+        .into_iter()
+        .skip(1)
+        .take(n - 2)
+        .map(|row| row.into_iter().skip(1).take(n-2).collect())
+        .collect();
+
+    Tile {
+        id: tile.id,
+        tile: cropped,
+    }
+}
+
+fn join(aligned: &Vec<u64>, tiles: &Vec<Tile>) -> Vec<Tile> {
+    aligned.iter()
+        .map(|id| tiles.iter().find(|tile| tile.id == *id).unwrap())
+        .cloned()
+        .map(crop)
+        .collect()
+}
+
 pub fn main() {
     let tiles = input(lines());
+    let size = (tiles.len() as f64).sqrt().trunc() as usize;
+
     let index = index(&tiles);
     let count = count(&tiles);
 
     let corners = corners(&index, &count);
     assert_eq!(corners.len(), 4);
-    let prod = corners.into_iter().product::<u64>();
+    let prod = corners.iter().product::<u64>();
     println!("{}", prod);
+
+    let aligned = corners.into_iter()
+        .map(|tile| {
+            let mut grid = Vec::with_capacity(size * size);
+            grid.push(tile);
+            let queue = tiles.iter()
+                .filter(|t| t.id != tile)
+                .cloned()
+                .map(|t| t.id)
+                .collect::<Vec<_>>();
+
+            align(size, grid, queue, 1, &index, &count)
+        })
+        .find(|a| !a.is_empty())
+        .unwrap_or_default();
+    assert_eq!(aligned.len(), tiles.len());
+
+    // let joined = join(&aligned, &tiles);
+    // TODO align each tile in the alignment
+    // TODO merge tiles into large single tile
+    // TODO search for pattern (flip/rotate if necessary)
 }
 
 #[cfg(test)]
@@ -164,7 +289,45 @@ mod tests {
     }
 
     #[test]
-    fn test_part1() {
+    fn test_align() {
+        let tiles = part1();
+        let size = (tiles.len() as f64).sqrt().trunc() as usize;
+
+        let index = index(&tiles);
+        let count = count(&tiles);
+
+        let corners = corners(&index, &count);
+        let aligned = corners.into_iter()
+            .map(|tile| {
+                let mut grid = Vec::with_capacity(size * size);
+                grid.push(tile);
+                let queue = tiles.iter()
+                    .map(|tile| tile.id)
+                    .clone()
+                    .into_iter()
+                    .filter(|t| *t != tile)
+                    .collect();
+
+                align(size, grid, queue, 1, &index, &count)
+            })
+            .find(|a| !a.is_empty())
+            .unwrap_or_default();
+
+        assert_eq!(aligned, vec![
+            1171, 1489, 2971,
+            2473, 1427, 2729,
+            3079, 2311, 1951,
+        ]);
+        //
+        // vec![
+        //     1951, 2311, 3079,
+        //     2729, 1427, 2473,
+        //     2971, 1489, 1171,
+        // ]
+    }
+
+    #[test]
+    fn test_corners() {
         let tiles = part1();
         let index = index(&tiles);
         let count = count(&tiles);
